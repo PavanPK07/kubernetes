aedcc83857258d9e2d7d03a431b61c2a02ee08f9
PavanPK07 <kalyanreddypallala@gmail.com>
2024-10-17 16:55:08 +0530

    k8

--- tf-aws-minikube/docker.sh
+++ tf-aws-minikube/docker.sh
@@ -79,3 +79,5 @@ curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 
 sudo mv /tmp/eksctl /usr/local/bin
+
+VALIDATE $? "eksctl installation"
